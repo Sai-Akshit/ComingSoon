3e3417fad28ef8f311c07af3ac3409efc93a85f4
AkshitUriti <akshituriti@intendcareer.com>
2024-08-13 20:09:29 +0530

    removed requestemail app

--- requestemail/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RequestemailConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'requestemail'
--- requestemail/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'requestemail'
-
-urlpatterns = [
-    path('', views.request_email, name='request_email'),
-]
